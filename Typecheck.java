f1867d9f06271117360fd3fdcaf1c76189a68d87
Steven Calvert <sdc977@gmail.com>
2021-01-11 18:53:01 -0800

    Back to the drawing board.

--- Typecheck.java
+++ Typecheck.java
@@ -1,17 +1,11 @@
-import visitor.DepthFirstVisitor;
 import syntaxtree.*;
 import java.util.*;
 
-public class Typecheck extends DepthFirstVisitor {
+public class Typecheck {
     public static void main(String args[]) {
         try {
             new MiniJavaParser(System.in);
             Goal mainnode = MiniJavaParser.Goal(); // This is the TOP node of our syntax tree. Pass around as needed.
-            TypeCheckHelper checker = new TypeCheckHelper();
-            checker.visit(mainnode);
-            if(!checker.Goalcheck(mainnode)) {
-                throw new Error("Type checking failed.");
-            }
             System.out.println("Program type checked sucessfully");
         } catch (Throwable e) {
             //System.out.println("Syntax check failed: " + e.getMessage());
@@ -19,5 +13,3 @@ public class Typecheck extends DepthFirstVisitor {
         }
     }
 }
-
-// We are going to make our own class that EXTENDS the given visitors.
