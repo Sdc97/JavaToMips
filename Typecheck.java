b3267131f733fefc571e989954a9c80373519545
Steven Calvert <sdc977@gmail.com>
2021-01-11 19:37:00 -0800

    Typo in main

--- Typecheck.java
+++ Typecheck.java
@@ -6,7 +6,7 @@ public class Typecheck {
         try {
             new MiniJavaParser(System.in);
             Goal mainnode = MiniJavaParser.Goal(); // This is the TOP node of our syntax tree. Pass around as needed.
-            System.out.println("Program type checked sucessfully");
+            System.out.println("Program type checked successfully");
         } catch (Throwable e) {
             //System.out.println("Syntax check failed: " + e.getMessage());
             System.out.println("Type error");
