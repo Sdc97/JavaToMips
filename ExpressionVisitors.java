10a9334bfcfbf48be26d81462fb0ed2c6f3ceb8a
houshmandX <houshmand.esmaeilpour@gmail.com>
2021-01-15 19:12:25 -0800

    updated expressionvisitor

--- ExpressionVisitors.java
+++ ExpressionVisitors.java
@@ -79,7 +79,7 @@ public class ExpressionVisitors extends GJDepthFirst<String,ContextType> {
      public String visit(ArrayLookup n, ContextType argu) {
         String p1 = n.f0.accept(this, argu);
         String p2 = n.f2.accept(this, argu);
-        if(p1.equals("int[]") && p1.equals(p2)) {
+        if(p1.equals("int[]") && p2.equals("int")) {
             return p1;
         }
         throw new Error("Type error");
@@ -91,17 +91,17 @@ public class ExpressionVisitors extends GJDepthFirst<String,ContextType> {
     * f2 -> "length"
     */
      public String visit(ArrayLength n, ContextType argu) {
-        String p1 = n.f0.accept(this, argu);
-        //String p2 = n.f2.accept(this, argu);
-        if(p1.equals("int[]")) {
-            return p1;
+        String _ret = null;
+        String ptype = argu.getTypeEnvType(n.f0.f0.tokenImage);
+        if(!ptype.equals("int[]")) {
+            throw new Error("Type error");
         }
-        throw new Error("Type error");
+        return "int";
      }
      /*
       TODO: #35
      */
-     
+
      /**
     * f0 -> IntegerLiteral()
     *       | TrueLiteral()
@@ -113,9 +113,123 @@ public class ExpressionVisitors extends GJDepthFirst<String,ContextType> {
     *       | NotExpression()
     *       | BracketExpression()
     */
-   // public String visit(PrimaryExpression n, ContextType argu) {
 
-    // }
-      
-   
+     public String visit(PrimaryExpression n, ContextType argu) {
+       
+        String _ret = null;
+        _ret = n.f0.accept(this,argu);
+        return _ret;
+     }
+     
+     /**
+    * f0 -> <INTEGER_LITERAL>
+    */
+     public String visit(IntegerLiteral n) {
+
+        String _ret = null;
+        //String t1 = n.f0.accept(this);
+        _ret = "int";
+        return _ret;
+     }
+     /**
+    * f0 -> "true"
+    */
+     public String visit(TrueLiteral n) {
+
+        String _ret = null;
+        //String t1 = n.f0.accept(this);
+        _ret = "boolean";
+        return _ret;
+     }
+     /**
+    * f0 -> "false"
+    */
+     public String visit(FalseLiteral n) {
+
+        String _ret = null;
+        //String t1 = n.f0.accept(this);
+        _ret = "boolean";
+        return _ret;
+     }
+     //TODO #39
+     /**
+    * f0 -> <IDENTIFIER>
+    */
+     public String visit(Identifier n) {
+
+        String _ret = null;
+        //String t1 = n.f0.accept(this);
+        return _ret;
+     }
+     //TODO #40
+     /**
+    * f0 -> "this"
+    */
+    public String visit(ThisExpression n) {
+
+        String _ret = null;
+        //String t1 = n.f0.accept(this);
+        return _ret;
+     }
+     /**
+    * f0 -> "new"
+    * f1 -> "int"
+    * f2 -> "["
+    * f3 -> Expression()
+    * f4 -> "]"
+    */
+     public String visit(ArrayAllocationExpression n, ContextType argu) {
+
+        String _ret = null;
+        String Atype = argu.getTypeEnvType(n.f0.f0.tokenImage);
+        if(!Atype.equals("int")) {
+            throw new Error("Type error");
+        }
+        return "int[]";
+     }
+     //TODO #42
+     /**
+    * f0 -> "new"
+    * f1 -> Identifier()
+    * f2 -> "("
+    * f3 -> ")"
+    */
+     public String visit(AllocationExpression n) {
+
+        String _ret = null;
+        //String t1 = n.f0.accept(this);
+        return _ret;
+     }
+     /**
+    * f0 -> "!"
+    * f1 -> Expression()
+    */
+     public String visit(NotExpression n, ContextType argu) {
+        String _ret = null;
+        n.f0.accept(this,argu);
+        _ret = n.f1.accept(this, argu);
+        if(!_ret.equals("boolean"))
+        {
+            throw new Error("Type error");
+        }
+        return _ret;
+     }
+     /**
+    * f0 -> "("
+    * f1 -> Expression()
+    * f2 -> ")"
+    */
+     public String visit(BracketExpression n, ContextType argu) {
+
+        String _ret = null;
+        n.f0.accept(this, argu);
+        _ret = n.f1.accept(this, argu);
+        n.f2.accept(this, argu);
+        if(!_ret.equals("boolean"))
+        {
+            throw new Error("Type error");
+        }
+        return _ret;
+     }
+
 }
