d32b785c44682f90efddaf1a45e626257c5af1d5
Steven Calvert <sdc977@gmail.com>
2021-02-01 18:50:40 -0800

    Removed references to Vapor visitor.

--- Phase2/J2V.java
+++ Phase2/J2V.java
@@ -6,7 +6,6 @@ public class J2V {
         try {
             new MiniJavaParser(System.in);
             Goal mainnode = MiniJavaParser.Goal(); // This is the TOP node of our syntax tree. Pass around as needed.
-            System.out.println(mainnode.accept(new VaporPrintVisitor()));
         } catch (Throwable e) {
             System.out.println(e.getMessage());
             System.exit(1);
