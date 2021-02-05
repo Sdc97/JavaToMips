9555b8fba29066eba75f3834eb08725e08445f88
Steven Calvert <sdc977@gmail.com>
2021-02-04 16:19:33 -0800

    Early v-table structure done

--- Phase2/J2V.java
+++ Phase2/J2V.java
@@ -7,7 +7,7 @@ public class J2V {
             new MiniJavaParser(System.in);
             Goal mainnode = MiniJavaParser.Goal(); // This is the TOP node of our syntax tree. Pass around as needed.
             mainnode.accept(new UpperLevelVisitor(), null);
-            VTableCreator.ClassTopSort();
+            VTableCreator.CreateVTables();
         } catch (Throwable e) {
             System.out.println(e.getMessage());
             System.exit(1);
--- Phase2/VTableCreator.java
+++ Phase2/VTableCreator.java
@@ -5,10 +5,42 @@ public class VTableCreator {
     public static String CreateVTables() {
         String result = "";
         ClassTopSort();
-        Map<String,String> vtables = new HashMap<>();
+        Map<String,List<List<String>>> vtables = new HashMap<>();
         for(String item: toporder) {
-            
+            if(item.equals(ContextType.mainClass)) continue;
+            // parent -> ((A,run), (B,runB))
+            // C has runB
+            // C -> ((A,run), (C,runB))
+            String parent = ContextType.class_parents.get(item);
+            if(parent.equals("")) { // In this loop classes that have no parent
+                List<List<String>> curr = new Vector<>();
+                vtables.put(item, curr);
+                
+                for(String methodname : ContextType.class_to_methods.get(item).keySet()) {
+                    List<String> tmp = new Vector<>();
+                    tmp.add(item);
+                    tmp.add(methodname);
+                    curr.add(tmp);
+                }
+            } else {
+                List<List<String>> parentvtable = vtables.get(parent);
+                List<List<String>> curr = new Vector<>();
+                vtables.put(item,curr);
+                for(List<String> methodpair : parentvtable) {
+                    if(!ContextType.class_to_methods.get(item).containsKey(methodpair.get(1))) {
+                        curr.add(methodpair);
+                    }
+                }
+
+                for(String methodname : ContextType.class_to_methods.get(item).keySet()) {
+                    List<String> tmp = new Vector<>();
+                    tmp.add(item);
+                    tmp.add(methodname);
+                    curr.add(tmp);
+                }
+            }
         }
+        System.out.println(vtables);
         return result;
     }
 
