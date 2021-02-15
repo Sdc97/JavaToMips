511830e146d733dff9063942c669e2043e4db5a6
Steven Calvert <scalv012@bolt.cs.ucr.edu>
2021-02-15 11:32:53 -0800

    Vapor parsing, interval creation

--- Phase3/V2VM.java
+++ Phase3/V2VM.java
@@ -1,9 +1,10 @@
-/*
+
 import cs132.util.ProblemException;
 import cs132.vapor.parser.VaporParser;
 import cs132.vapor.ast.VaporProgram;
 import cs132.vapor.ast.VBuiltIn.Op;
-*/
+import cs132.vapor.ast.*;
+import cs132.vapor.parser.*;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.io.PrintStream;
@@ -11,38 +12,43 @@ import java.io.*;
 import java.util.*;
 
 public class V2VM {
-/* **************copied from the project web page 
 
-public static VaporProgram parseVapor(InputStream in, PrintStream err) throws IOException {
-  Op[] ops = {
-    Op.Add, Op.Sub, Op.MulS, Op.Eq, Op.Lt, Op.LtS,
-    Op.PrintIntS, Op.HeapAllocZ, Op.Error,
-  };
-  boolean allowLocals = true;
-  String[] registers = null;
-  boolean allowStack = false;
+  public static VaporProgram parseVapor(InputStream in, PrintStream err) throws IOException {
+    Op[] ops = {
+      Op.Add, Op.Sub, Op.MulS, Op.Eq, Op.Lt, Op.LtS,
+      Op.PrintIntS, Op.HeapAllocZ, Op.Error,
+    };
+    boolean allowLocals = true;
+    String[] registers = null;
+    boolean allowStack = false;
 
-  VaporProgram tree;
-  try {
-    tree = VaporParser.run(new InputStreamReader(in), 1, 1,
-                           java.util.Arrays.asList(ops),
-                           allowLocals, registers, allowStack);
-  }
-  catch (ProblemException ex) {
-    err.println(ex.getMessage());
-    return null;
-  }
-
-  return tree;
-}
-*/
-
-public static void main(String args[]) {
+    VaporProgram tree;
     try {
-       
-    } catch (Throwable e) {
-        System.out.println(e.getMessage());
-        System.exit(1);
+      tree = VaporParser.run(new InputStreamReader(in), 1, 1,
+                            java.util.Arrays.asList(ops),
+                            allowLocals, registers, allowStack);
     }
-}
+    catch (ProblemException ex) {
+      err.println(ex.getMessage());
+      return null;
+    }
+
+    return tree;
+  }
+
+  public static void main(String args[]) {
+      try {
+          VaporProgram temp = parseVapor(System.in, System.out);
+          for(int i = 0; i < temp.dataSegments.length; i++) {
+            VDataSegment vdata = temp.dataSegments[i];
+            System.out.println(vdata.ident);
+            for(int j = 0; j < vdata.values.length; j++) {
+              System.out.println("\t" + vdata.values[j]);
+            }
+          }
+      } catch (Throwable e) {
+          System.out.println(e.getMessage());
+          System.exit(1);
+      }
+  }
 }
